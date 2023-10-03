0b137db3257dc000f9498647020422576b9d81bd
mklee <iamdevstar@gmail.com>
2023-10-03 18:10:53 +0900

    add money-service module

---
+++ money-service/src/main/java/com/xpay/money/MoneyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xpay.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MoneyServiceApplication.class, args);
+  }
+
+}
