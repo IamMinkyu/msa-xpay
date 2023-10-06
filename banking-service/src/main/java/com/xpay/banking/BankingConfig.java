0c20931e91b571d47e7a58806ee2a356447fbe64
mklee <iamdevstar@gmail.com>
2023-10-06 14:06:50 +0900

    add LoggingAspect, LoggingProducer

---
+++ banking-service/src/main/java/com/xpay/banking/BankingConfig.java
@@ -0,0 +1,10 @@
+package com.xpay.banking;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.xpay.common")
+public class BankingConfig {
+
+}
---
+++ common/src/main/java/com/xpay/common/LoggingProducer.java
@@ -0,0 +1,40 @@
+package com.xpay.common;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+public class LoggingProducer {
+
+  private final KafkaProducer<String, String> producer;
+  private final String topic;
+
+  public LoggingProducer(@Value("${kafka.cluster.bootstrapservers}") String bootstrapServers,
+                         @Value("${logging.topic}") String topic) {
+
+    Properties props = new Properties();
+    props.put("bootstrap.servers", bootstrapServers);
+    props.put("key.serialize", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serialize", "org.apache.kafka.common.serialization.StringSerializer");
+
+    this.producer = new KafkaProducer<>(props);
+    this.topic = topic;
+  }
+
+  public void sendMessage(String key, String value) {
+    ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+    producer.send(record, (metadata, exception) -> {
+      if (exception == null) {
+        System.out.println("Success to send message: " + metadata);
+      }
+      else {
+        exception.printStackTrace();
+      }
+    });
+  }
+
+}
