bd76b55f1a8a567252d3c7a92a49cfcc77224f2f
mklee <iamdevstar@gmail.com>
2023-10-11 17:39:45 +0900

    chor: 다시 시작해보자

--- common/src/main/java/com/xpay/common/LoggingAspect.java
+++ common/src/main/java/com/xpay/common/LoggingAspect.java
@@ -1,22 +1,17 @@
 package com.xpay.common;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
-
-@Aspect
-@Component
-public class LoggingAspect {
-  private final LoggingProducer loggingProducer;
-
-  public LoggingAspect(LoggingProducer loggingProducer) {
-    this.loggingProducer = loggingProducer;
-  }
-  @Before("execution(* com.xpay.*.adapter.in.web.*.*(..))")
-  public void beforeMethodExecution(JoinPoint joinPoint) {
-    String methodName = joinPoint.getSignature().getName();
-    loggingProducer.sendMessage("Logging", "Before execution method: " + methodName);
-    // Produce Access Log
-  }
-}
+//@Aspect
+//@Component
+//public class LoggingAspect {
+//  private final LoggingProducer loggingProducer;
+//
+//  public LoggingAspect(LoggingProducer loggingProducer) {
+//    this.loggingProducer = loggingProducer;
+//  }
+//  @Before("execution(* com.xpay.*.adapter.in.web.*.*(..))")
+//  public void beforeMethodExecution(JoinPoint joinPoint) {
+//    String methodName = joinPoint.getSignature().getName();
+//    loggingProducer.sendMessage("Logging", "Before execution method: " + methodName);
+//    // Produce Access Log
+//  }
+//}
